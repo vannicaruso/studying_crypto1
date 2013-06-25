f916bc4a5c7bf95b7beaa383d0d2c1bfbef5e9e8
Giovanni Caruso <vanni.caruso@gmail.com>
2013-06-26 01:31:50 +0200

    Caricamento iniziale

---
+++ src/it/vannicaruso/crypto/HEX.scala
@@ -0,0 +1,45 @@
+package it.vannicaruso.crypto
+
+
+/**
+ * La classe che rappresenta una stringa esadecimale
+ * @param hex
+ */
+class HEX(val hex: String){
+  import HEX._
+  /*making xor*/
+  def xor(that: String) =
+    byte_array_to_hex(xor_byte_array(hex_to_byte_array(hex), hex_to_byte_array(that)))
+}
+
+
+
+object HEX {
+  implicit def string2HEX(s: String) = new HEX(s)
+  /**
+   * XOR tra 2 byte
+   * @param b1 byte 1
+   * @param b2 byte 2
+   * @return il byte che rappresenta lo XOR tra b1 e b2
+   */
+  private def xor_byte(b1: Byte, b2: Byte) = (b1 ^ b2).toByte
+
+  /**
+   * Esegue lo XOR tra due array di byte e restituisce l'array di byte corrispondenti
+   * @param b1  il primo array di byte
+   * @param b2  il secondo array di byte
+   * @return l'array che costituisce lo XOR
+   */
+  private def xor_byte_array(b1: Array[Byte], b2: Array[Byte]) =
+    (b1 zip b2).foldLeft(Array[Byte]()){(acc,actual) => acc :+ xor_byte(actual._1, actual._2)}
+
+  /**
+   * Trasforma una stringa esadecimale nel corrispondente array di byte
+   * @param s la stringa esadecimale
+   * @return  l'array di byte corrispondente
+   */
+  private def hex_to_byte_array(s: String) = s.sliding(2,2).map{Integer.parseInt(_,16).toByte}.toArray
+
+  private def byte_array_to_hex (a: Array[Byte]) = new String(a)
+
+}
